0c027f64a0a73b57097dfa5b9bb5de7ce585b6aa
Haisheng Wu <haisheng.wu@okta.com>
2019-03-01 22:05:02 -0800

    alright, hoxy is still awesome!!!

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 0. install `yarn`
 1. `yarn install`
-2. start server like `OKTA_SERVER={your-okta-domain} PORT=4001 yarn start`
+2. start server like `OKTA_SERVER=https://your-okta-domain.okta.com PORT=4001 yarn start`
--- app.js
+++ app.js
@@ -1,68 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var proxy = require('http-proxy-middleware');
+const proxyServer = process.env.OKTA_SERVER;
+const hoxy = require('hoxy');
+const proxy = hoxy.createServer({
+  reverse: proxyServer,
+});
 
-var proxyServer = process.env.OKTA_SERVER;
-//
-// TODO: failed to proxy /api/v1/authn request
-//
-var oktaServer = proxy({
-  context: ['/api', '/oauth2'],
-  target: proxyServer,
-  changeOrigin: true,
-  followRedirects: true,
-  onProxyReq(proxyReq, req) {
-    console.log(`Proxying request: ${proxyServer}${req.url}`);
-  },
-  onProxyRes(proxyRes, req, res) {
-    console.log(`Proxying get response: requrl (${req.url})`);
-    console.log(proxyRes);
-    /*
-      if (req.url === '/oauth2/v1') {
-        // delete id token
-      }
-    */
-    //console.log(JSON.stringify(proxyRes, null, 2));
-  },
-  onError(err, req, res) {
-    console.error('proxy server got error', err);
+proxy.intercept({
+  phase: 'response',
+  url: '/oauth2/*',
+  mimeType: 'application/json',
+  as: 'json'
+}, function(req, resp, cycle) {
+  console.log(req.url);
+  if (req.url.indexOf('/v1/token') > 0) {
+    console.log('remove id_token from token response');
+    delete resp.json.id_token;
+    console.log(resp.json);
   }
 });
 
-var indexRouter = require('./routes/index');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/', oktaServer);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+proxy.listen(process.env.PORT);
